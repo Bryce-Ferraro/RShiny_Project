8a6df4b44e5a732832d0e70f72ff6f522dbbf5af
Bryce-Ferraro <ferrarobryce@gmail.com>
2024-01-24 18:51:05 -0300

    Update RShiny_Project

--- RShiny_Project/app.R
+++ RShiny_Project/app.R
@@ -7,44 +7,61 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+
+library(shiny) 
+library(shinydashboard)
+library(data.table) 
+library(readxl)
+library(reshape2)
+library(ggplot2) 
+library(ggpmisc)
+library(plotly)
+library(dplyr) 
+library(readr)
+library(DT) 
+library(shinyWidgets) 
+library(shinyjs) 
+library(stringr) 
+library(shinythemes)
+library(dbplyr)
+library(tidyverse)
+library(igraph) 
+library(gtsummary)
+library(gt)
+
+df_dsp_purif_stream_results_entity = read.table(file = "Stream_Results v240124 BBF.txt", header = TRUE, sep = ',')
+
+df_dsp_purif_stream_results_entity_product = df_dsp_purif_stream_results_entity %>%
+  filter(stream == 'Product')
+
+df_dsp_filter_ts = read.table(file = "TS_Query v2 v240124 BBF.txt", header = TRUE, sep = ',')
+
+
+df_dsp_membrane_log_results_entity = read.table(file = "Membrane v240124 BBF.txt", header = TRUE, sep = ',')
+
+
+
+pink_shades <- c(
+  "#FF6961","#FFC0CB","#DB7093","#F6653B","#E36C75","#DB7093", "#C71585","#FFBDC8","#FF3EA3","#FF621E", 
+  "#DB7093","#FFBBC5" ,"#FFB8C3" ,"#FFB6C1" ,"#FFA2BD" ,"#FF8FBA", "#FF7CB7", "#FF69B4", "#FF53AB" ,
+  "#FF299B" ,"#FF1493", "#F52B93", "#ED4193" ,"#E35993", "#DB7093", "#D6598F", "#D1428C","#CC2B88",
+  "#F6653B","#ED6958","#E36C75","#DB7093"
+)
+
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+   
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    
 }
 
 # Run the application 
